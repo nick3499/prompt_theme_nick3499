feec5471cf8f26094dcdf3d3feee9fe8aa1ff193
nick3499 <ntrinen@yahoo.com>
2020-02-17 16:13:02 -0600

    Revised path
    obfuscated local path

--- .prompt_theme.sh
+++ .prompt_theme.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 export TERM=xterm-256color;
-export PROMPT_COMMAND='PS1="$(python3 /home/nick/.userpy/prompt_theme/.prompt_theme.py)"'
+export PROMPT_COMMAND='PS1="$(python3 /home/foo/scripts/prompt_theme/.prompt_theme.py)"'
